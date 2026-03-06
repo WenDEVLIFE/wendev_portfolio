64eeded9fb4b14b941fed0c86deb348a276ca0d0
WenDEVLIFE <frouenmedinajr@gmail.com>
2026-03-06 16:36:57 +0800

    added backgrouhd

---
+++ components/ui/Background.tsx
@@ -0,0 +1,8 @@
+export function Background() {
+    return (
+        <>
+            <div className="noise" />
+            <div className="fixed -top-[20vh] left-[50vw] w-[100vw] h-[50vh] -translate-x-1/2 bg-[radial-gradient(ellipse_at_center,rgba(255,255,255,0.04)_0%,transparent_60%)] -z-10 pointer-events-none" />
+        </>
+    );
+}
